862bc53fe16d7b5b7696e7f4bf9f8061258db211
Marc-Velay <marc.velay@gmail.com>
2017-07-12 17:52:38 +0200

    modified docker files #44

--- wait-for-it.sh
+++ wait-for-it.sh
@@ -7,12 +7,13 @@ host="$1"
 shift
 cmd="$@"
 
-until mysqladmin ping -h localhost
-do
-  echo "Waiting for database connection..."
+#until mysqladmin ping -h localhost
+#do
+#  echo "Waiting for database connection..."
   # wait for 5 seconds before check again
-  sleep 5
-done
+#  sleep 5
+#done
+sleep 1000
 
 >&2 echo "MySQL is up - executing command"
 exec $cmd
